b874900fd12aa147a7a2920e2f49ec6137de2a31
mathias <mdi@pygmatec.com>
2017-09-22 11:27:27 +0200

    add callback failure

--- roscommunication.py
+++ roscommunication.py
@@ -13,15 +13,16 @@ PY2 = False
 
 class _ClientThread(threading.Thread):
 
-    def __init__(self, message, callback=None,timeout=None, singleResponse = False):
+    def __init__(self, message, callbackSuccess=None, callbackFailure=None,timeout=None, singleResponse = False):
 
-        ip="10.0.0.129"
+        ip="10.0.0.128"
         port=9090
 
         threading.Thread.__init__(self)
         self.ip = ip
         self.port = port
-        self.callback = callback
+        self.callbackSuccess = callbackSuccess
+        self.callbackFailure = callbackFailure
         self.timeout = timeout
         self.singleResponse = singleResponse
 
@@ -34,48 +35,81 @@ class _ClientThread(threading.Thread):
         self.start()
 
     def run(self):
-
-        self.clientsocket.connect((self.ip, self.port))
+        try:
+            self.clientsocket.connect((self.ip, self.port))
+        except Exception as e:
+            if(self.callbackFailure):
+                self.callbackFailure(e)
+            else:
+                print("Socket connection failure : "+str(e))
+            return
         print("Connection: %s %s" % (self.ip, self.port ))
 
-        self.clientsocket.send(self.message)
 
-        if(self.callback):
+        try:
+            self.clientsocket.send(self.message)
+        except Exception as e:
+            if(self.callbackFailure):
+                self.callbackFailure(e)
+            else:
+                print("Socket send failure"+str(e))
+            return
+
+
+        if(self.callbackSuccess):
             if(self.timeout):
                 self.clientsocket.settimeout(self.timeout)
 
             self.do_run = True
             while(self.do_run):
-                r = self.clientsocket.recv(2048).decode("utf-8")
-                if(not r == "") : self.callback(r)# if(r is not None) else pass
+                r = None
+                try:
+                    r = self.clientsocket.recv(2048).decode("utf-8")
+                except Exception as e:
+                    if(self.callbackFailure):
+                        self.callbackFailure(e)
+                    else:
+                        print("Socket receive failure"+str(e))
+                    return
+
+
+                if(not r == "") : self.callbackSuccess(r)# if(r is not None) else pass
                 if(self.singleResponse):
                     self.do_run = False
 
 
         self.clientsocket.close()
 
+
+
 class ROS_TopicPublisher(_ClientThread):
-    def __init__(self,topic,type,msg):
+    def __init__(self,topic,type,msg, callbackFailure=None):
         super(ROS_TopicPublisher,self).__init__('{"op":"publish","topic":"'+topic+'","msg":'+msg+'}')
 
 class ROS_TopicSubscriber(_ClientThread):
-    def __init__(self,topic,callback):
-        super(ROS_TopicSubscriber,self).__init__('{"op":"subscribe","topic":"'+topic+'"}',callback=callback)
+    def __init__(self,topic,callback, callbackFailure=None):
+        super(ROS_TopicSubscriber,self).__init__('{"op":"subscribe","topic":"'+topic+'"}',callbackSuccess=callback)
     def unsuscribe(self):
         self.do_run = False
         self.clientsocket.shutdown(socket.SHUT_WR)
 
 class ROS_ServiceCaller(_ClientThread):
-    def __init__(self,service,callback, args=None):
+    def __init__(self,service,callback, args=None, callbackFailure=None):
         arguments = "" if args is None else ',"args":'+json.dumps(args)#str(args).replace("'","\"")
         print("arguments: '"+arguments+"'")
-        super(ROS_ServiceCaller,self).__init__('{"op":"call_service","service":"'+service+'"'+arguments+'}',callback=callback, singleResponse=True)
+        super(ROS_ServiceCaller,self).__init__('{"op":"call_service","service":"'+service+'"'+arguments+'}',callbackSuccess=callback, callbackFailure=callbackFailure, singleResponse=True)
+
+
 
 if(__name__=="__main__"):
 
     def callbackReceive(recv):
         print("\n\nReceive:")
         print(str(recv)+"\n")
+
+    def callbackFailure(e):
+        print("\n\Failure:")
+        print(str(e)+"\n")
 
     def callbackReceivePrograms(recv):
         print("\n\nReceive programs:")
